d638d9eba731b1758171cc5544ab2b5a00255d98
Robin.Wang <3Jg$94sbQ@q1>
2023-05-26 16:01:05 +0800

    style(refrence)

--- ui.py
+++ ui.py
@@ -141,7 +141,7 @@ with st.form("form", True):
         source = "\n\n"
         source += "".join(
             [
-                f"""> *出处[{i + 1}]{os.path.split(doc.metadata['source'])[-1]}*\n\n"""
+                f"""> *出处[{i + 1}] {os.path.split(doc.metadata['source'])[-1]}*\n\n"""
                 for i, doc in
                 enumerate(q["source_documents"])])
         st.session_state.ctx = predict(q['result'], source, st.session_state.ctx)
