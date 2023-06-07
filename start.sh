80bcfc8cea5ddab3a7728313a3940f77d93505ae
Robin.Wang <3Jg$94sbQ@q1>
2023-06-07 15:22:11 +0800

    change script

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-streamlit run ui.py --browser.gatherUsageStats False --server.port 6006
+#streamlit run ui.py --browser.gatherUsageStats False --server.port 6006
+streamlit run index.py --browser.gatherUsageStats False --server.port 6006
